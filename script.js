03a5eda5dcf215197d44d8c8b26971e495c195fb
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-27 11:55:11 -0700

    this is the answer of pluses-and-hashes

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Good Luck 💪🏾
+function plusHashCount(str) {
+    var hashCount = 0;
+    var plusCount = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '#') {
+        hashCount++;
+      } else if (str[i] === '+') {
+        plusCount++;
+      }
+    }
+    
+    return {
+      hashes: hashCount,
+      pluses: plusCount
+    };
+  }
+console.log(plusHashCount("###+")); 
+console.log(plusHashCount("##+++#"));  
+console.log(plusHashCount("#+++#+#++#")); 
+console.log(plusHashCount(""));  
